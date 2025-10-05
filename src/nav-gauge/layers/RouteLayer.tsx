e93d797807782b6a7cc47324258f59009af7c32d
spookydoodle <kxkaro@gmail.com>
2025-10-05 15:46:40 +0200

    Add current point marker

--- src/nav-gauge/layers/RouteLayer.tsx
+++ src/nav-gauge/layers/RouteLayer.tsx
@@ -1,4 +1,7 @@
-import { FC, useCallback, useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
+import turfAlong from "@turf/along";
+import * as turfHelpers from "@turf/helpers";
+import turfLength from "@turf/length";
 import { useMap } from "../../map/useMap";
 import { GeoJson } from "../../parsers";
 import { useGaugeSettings } from "../../gauge-settings/use-gauge-settings";
@@ -11,49 +14,76 @@ export interface RouteTimes {
     duration: number;
 }
 
+const colorActive = '#003161';
+const colorInactive = 'grey';
+
 const sourceId = 'route';
 
 const sourceIds = {
+    currentPoint: sourceId + '-current-point',
     line: sourceId + '-line',
 }
 
 const layerIds = {
+    currentPointOutline: 'route-current-point-outline',
+    currentPoint: 'route-current-point',
     points: 'route-points',
     line: 'route-line'
 }
+
+const getCurrentPoint = (
+    geojson: GeoJson,
+    splitIndex: number,
+    currentTime: number
+) => {
+    const currentLineStart = geojson.features[Math.max(0, splitIndex - 1)];
+    const currentLineEnd = geojson.features[splitIndex];
+    const currentLineStartTime = new Date(currentLineStart.properties.time).valueOf();
+    const currentLineEndTime = new Date(currentLineEnd.properties.time).valueOf();
+
+    const fraction = Number(((currentTime - currentLineStartTime) / (currentLineEndTime - currentLineStartTime)).toFixed(2));
+    const currentLineStartPos = currentLineStart.geometry.coordinates;
+    const currentLineEndPos = currentLineEnd.geometry.coordinates;
+    const line = turfHelpers.lineString([currentLineStartPos, currentLineEndPos]);
+    const totalDistance = turfLength(line, { units: 'meters' });
+
+    return turfAlong(line, totalDistance * fraction, { units: 'meters' });
+};
 
 const getData = (
     geojson: GeoJson,
     startTimeEpoch: number,
     progressMs: number
-): GeoJSON.GeoJSON => {
-    const splitIndex = geojson.features.findIndex((f) => new Date(f.properties.time).valueOf() > new Date(startTimeEpoch + progressMs).valueOf());
+): [GeoJSON.Feature<GeoJSON.Point>, GeoJSON.GeoJSON] => {
+    const currentTime = startTimeEpoch + progressMs;
+    const splitIndex = geojson.features.findIndex((f) => new Date(f.properties.time).valueOf() > new Date(currentTime).valueOf());
+    const currentPoint = getCurrentPoint(geojson, splitIndex, currentTime);
 
-    return {
+    return [currentPoint, {
         ...geojson,
         features: [
             {
                 type: 'Feature',
                 geometry: {
                     type: 'LineString',
-                    coordinates: geojson.features.slice(0, splitIndex).map((f) => f.geometry.coordinates)
+                    coordinates: geojson.features.slice(0, splitIndex).map((f) => f.geometry.coordinates).concat([currentPoint.geometry.coordinates])
                 },
                 properties: {
-                    status: 'before'
+                    status: 'before',
                 }
             },
             {
                 type: 'Feature',
                 geometry: {
                     type: 'LineString',
-                    coordinates: geojson.features.slice(splitIndex).map((f) => f.geometry.coordinates)
+                    coordinates: [currentPoint.geometry.coordinates].concat(geojson.features.slice(splitIndex).map((f) => f.geometry.coordinates))
                 },
                 properties: {
-                    status: 'after'
+                    status: 'after',
                 }
             },
         ]
-    };
+    }];
 };
 
 interface Props {
@@ -74,12 +104,18 @@ export const RouteLayer: FC<Props> = ({
     const { map } = useMap();
     const { showRouteLine, showRoutePoints } = useGaugeSettings();
     const [isLayerAdded, setIsLayerAdded] = useState(false);
+    const [currentPoint, lines] = getData(geojson, routeTimes.startTimeEpoch, progressMs);
 
     useEffect(() => {
+        map.addSource(sourceIds.currentPoint, {
+            type: 'geojson',
+            data: currentPoint,
+        });
+
         if (showRouteLine || showRoutePoints) {
             map.addSource(sourceIds.line, {
                 type: 'geojson',
-                data: getData(geojson, routeTimes.startTimeEpoch, progressMs),
+                data: lines,
             });
         }
 
@@ -92,11 +128,15 @@ export const RouteLayer: FC<Props> = ({
                     'line-color': [
                         'case',
                         ['==', ['get', 'status'], 'before'],
-                        'black',
-                        'grey'
+                        colorActive,
+                        colorInactive
                     ],
                     'line-width': 4,
                     'line-opacity': .6,
+                },
+                layout: {
+                    'line-cap': 'round',
+                    'line-join': 'round'
                 }
             });
         }
@@ -110,13 +150,32 @@ export const RouteLayer: FC<Props> = ({
                     'circle-color': [
                         'case',
                         ['==', ['get', 'status'], 'before'],
-                        'black',
-                        'grey'
+                        colorActive,
+                        colorInactive
                     ],
                     'circle-radius': 3,
                 }
             });
         }
+
+        map.addLayer({
+            id: layerIds.currentPointOutline,
+            source: sourceIds.currentPoint,
+            type: 'circle',
+            paint: {
+                'circle-color': 'white',
+                'circle-radius': 7,
+            }
+        });
+        map.addLayer({
+            id: layerIds.currentPoint,
+            source: sourceIds.currentPoint,
+            type: 'circle',
+            paint: {
+                'circle-color': colorActive,
+                'circle-radius': 5,
+            }
+        });
 
         setIsLayerAdded(true);
 
@@ -148,7 +207,9 @@ export const RouteLayer: FC<Props> = ({
             if (startTimeEpoch + current > endTimeEpoch) {
                 current = 0;
             }
-            map.getSource<maplibregl.GeoJSONSource>(sourceIds.line)?.setData(getData(geojson, startTimeEpoch, current));
+            const [currentPoint, lines] = getData(geojson, startTimeEpoch, current);
+            map.getSource<maplibregl.GeoJSONSource>(sourceIds.currentPoint)?.setData(currentPoint);
+            map.getSource<maplibregl.GeoJSONSource>(sourceIds.line)?.setData(lines);
             onProgressMsChange(current);
             animation = requestAnimationFrame(animate);
         };
@@ -163,16 +224,20 @@ export const RouteLayer: FC<Props> = ({
     }, [isPlaying, isLayerAdded]);
 
     useEffect(() => {
-        if (isPlaying) {
-            return;
-        }
-        const { startTimeEpoch } = routeTimes;
-        map.getSource<maplibregl.GeoJSONSource>(sourceIds.line)?.setData(getData(geojson, startTimeEpoch, progressMs));
     }, [progressMs]);
 
     useEffect(() => {
         if (progressMs === 0) {
-            map.getSource<maplibregl.GeoJSONSource>(sourceIds.line)?.setData(getData(geojson, routeTimes.startTimeEpoch, 0));
+            const [currentPoint, lines] = getData(geojson, routeTimes.startTimeEpoch, 0);
+            map.getSource<maplibregl.GeoJSONSource>(sourceIds.currentPoint)?.setData(currentPoint);
+            map.getSource<maplibregl.GeoJSONSource>(sourceIds.line)?.setData(lines);
+        }
+
+        if (!isPlaying) {
+            const { startTimeEpoch } = routeTimes;
+            const [currentPoint, lines] = getData(geojson, startTimeEpoch, progressMs);
+            map.getSource<maplibregl.GeoJSONSource>(sourceIds.currentPoint)?.setData(currentPoint);
+            map.getSource<maplibregl.GeoJSONSource>(sourceIds.line)?.setData(lines);
         }
     }, [progressMs]);
 
