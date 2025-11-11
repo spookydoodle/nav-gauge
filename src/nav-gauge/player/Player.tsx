7861a8441e9d10a10ba28e8e5314c47671b513b8
spookydoodle <kxkaro@gmail.com>
2025-11-11 08:19:07 +0100

    use slider input

--- src/nav-gauge/player/Player.tsx
+++ src/nav-gauge/player/Player.tsx
@@ -1,8 +1,8 @@
 import { CSSProperties, FC, useCallback, useEffect, useMemo, useState } from "react";
 import { RouteTimes } from "../layers/RouteLayer";
 import { GeoJson, ImageData } from "../../logic/parsers";
-import * as styles from './player.module.css';
 import { formatProgressMs, formatTimestamp } from "../../logic/route-times-utils";
+import * as styles from './player.module.css';
 
 interface Props {
     geojson?: GeoJson;
@@ -23,15 +23,6 @@ export const Player: FC<Props> = ({
     isPlaying,
     onIsPlayingChange,
 }) => {
-    const [dragState, setDragState] = useState<{
-        type: 'touch' | 'mouse';
-        isPlaying: boolean;
-        startProgressPercentage: number;
-        width: number;
-        startClientX: number;
-        currentClientX: number;
-    } | null>(null);
-
     const handlePlayClick = () => onIsPlayingChange((prev) => !prev);
 
     const progressPercentage = useMemo(
@@ -44,90 +35,12 @@ export const Player: FC<Props> = ({
         [progressMs, routeTimes?.duration]
     );
 
-    const startDragging = (type: 'mouse' | 'touch', clientX: number, event: React.MouseEvent | React.TouchEvent) => {
-        onIsPlayingChange(false);
-        setDragState({
-            type,
-            isPlaying,
-            startProgressPercentage: progressPercentage ?? 0,
-            width: event.currentTarget.parentElement?.clientWidth ?? 0,
-            startClientX: clientX,
-            currentClientX: clientX,
-        });
-    }
-
-    const drag = (clientX: number) => {
-        setDragState((prev) => prev ? { ...prev, currentClientX: clientX } : prev);
-    }
-
-    const stopDragging = () => {
-        onIsPlayingChange(!!dragState?.isPlaying);
-        setDragState(null);
-    }
-
-    const handleThumbMouseDown = (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
-        startDragging('mouse', event.clientX, event);
-    };
-
-    useEffect(() => {
-        if (dragState?.type !== 'mouse') {
+    const handleProgressChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (!routeTimes || isNaN(Number(event.target.value))) {
             return;
         }
-        const mouseMoveHandler = (event: MouseEvent) => {
-            drag(event.clientX);
-        };
-        const mouseUpHandler = () => {
-            stopDragging();
-            document.removeEventListener('mousemove', mouseMoveHandler);
-            document.removeEventListener('mouseup', mouseUpHandler);
-        };
-
-        document.addEventListener('mousemove', mouseMoveHandler);
-        document.addEventListener('mouseup', mouseUpHandler);
-
-        return () => {
-            document.removeEventListener('mousemove', mouseMoveHandler);
-            document.removeEventListener('mouseup', mouseUpHandler);
-        };
-
-    }, [dragState?.type]);
-
-    const handleThumbTouchStart = (event: React.TouchEvent<HTMLSpanElement>) => {
-        startDragging('touch', event.touches[0]?.clientX ?? 0, event);
-    };
-
-    useEffect(() => {
-        if (dragState?.type !== 'touch') {
-            return;
-        }
-        const touchMoveHander = (event: TouchEvent) => {
-        event.preventDefault();
-            drag(event.touches[0]?.clientX ?? 0);
-        };
-        const touchEndHandler = () => {
-            stopDragging();
-            document.removeEventListener('touchmove', touchMoveHander);
-            document.removeEventListener('touchend', touchEndHandler);
-        };
-
-        document.addEventListener('touchmove', touchMoveHander, { passive: false });
-        document.addEventListener('touchend', touchEndHandler);
-
-        return () => {
-            document.removeEventListener('touchmove', touchMoveHander);
-            document.removeEventListener('touchend', touchEndHandler);
-        };
-    }, [dragState?.type]);
-
-    useEffect(() => {
-        if (!dragState || !routeTimes) {
-            return;
-        }
-        const diffPercentage = Number(((dragState.currentClientX - dragState.startClientX) / dragState.width * 100).toFixed(0));
-        const nextPercentage = dragState.startProgressPercentage + diffPercentage;
-        const clampedPercentage = (Math.max(0, Math.min(100, nextPercentage)));
-        onProgressMsChange((clampedPercentage / 100) * routeTimes.duration);
-    }, [dragState?.currentClientX]);
+        onProgressMsChange(Number(event.target.value) / 100 * routeTimes.duration);
+    }
 
     const getPosition = useCallback(
         (featureId: number) => {
@@ -135,23 +48,31 @@ export const Player: FC<Props> = ({
             if (!feature || !routeTimes) {
                 return 0;
             }
-            return (new Date(feature.properties.time).valueOf() - new Date(routeTimes.startTime).valueOf()) / routeTimes.duration;
+            return (new Date(feature.properties.time).valueOf() - new Date(routeTimes.startTime).valueOf()) / routeTimes.duration * 100;
         }, [geojson]);
 
     return (
         <div className={styles.player}>
             <div className={styles.pictures}>
                 {images
-                .filter((image) => image.featureId !== undefined)
-                .map((image) => (
-                    <span key={image.id} className={styles['image-marker']} style={{ left: `${getPosition(image.featureId!).toFixed(0)}%` }} />
-                ))}
+                    .filter((image) => image.featureId !== undefined)
+                    .map((image) => (
+                        <span key={image.id} className={styles['image-marker']} style={{ left: `${getPosition(image.featureId!).toFixed(0)}%` }} />
+                    ))}
             </div>
-            <div className={styles.slider} style={{
-                '--track-complete': `${progressPercentage ?? 0}%`
-            } as CSSProperties}>
-                <span className={styles.thumb} onMouseDown={handleThumbMouseDown} onTouchStart={handleThumbTouchStart} />
-            </div>
+            <input
+                type="range"
+                value={progressMs}
+                min={0}
+                max={routeTimes?.duration ?? 1}
+                step={1}
+                onChange={handleProgressChange}
+                // TODO: Fix styles for all browsers
+                // className={styles['progress-slider']}
+                style={{
+                    '--track-complete': `${progressPercentage ?? 0}%`
+                } as CSSProperties}
+            />
             <div className={styles.buttons}>
                 <p className={styles.text}>
                     {formatProgressMs(progressMs)} ({progressPercentage !== undefined ? `${progressPercentage.toFixed(0)}%` : '-'})
