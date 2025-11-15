edf45ffe1d0e1e48e54e57586c249d52b0b15cac
spookydoodle <kxkaro@gmail.com>
2025-11-15 10:58:35 +0100

    update tsconfigs

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'cypress';
 
-module.exports = defineConfig({
+export default defineConfig({
     e2e: {
-        baseUrl: 'https://gauge.spookydoodle.com',
+        baseUrl: 'http://localhost:3000',
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
     },
-})
+});
